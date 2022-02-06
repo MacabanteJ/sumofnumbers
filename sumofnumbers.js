6fbee8cd09fe85d8a44fe2033b39ab6db2315e09
MacabanteJ <jerichomacabante@gmail.com>
2022-02-06 12:51:50 -1000

    all code for sumofnumbers.js

---
+++ sumofnumbers.js
@@ -0,0 +1,34 @@
+let testData = [1, 2, 3, 4];
+
+function sumFor(list) {
+  let sum = 0;
+  for (const num of list) {
+    sum += num;
+  }
+  return sum;
+}
+console.log(sumFor(testData));
+function sumWhile(list) {
+  let sum = 0;
+  let i = 0;
+  while (i < list.length) {
+    sum += list[i];
+    i++;
+  }
+  return sum;
+}
+console.log(sumWhile(testData));
+function sumRecursion(list) {
+  const currentNum = list.shift();
+  if (list.length > 0) {
+    return sumRecursion(list) + currentNum;
+  }
+  return currentNum;
+}
+console.log(sumRecursion(testData));
+testData = [1, 2, 3, 4];
+function sumTheSimpleWay(list) {
+  const sum = _.reduce(list, function (memo, num) { return memo + num; }, 0);
+  return sum;
+}
+console.log(sumTheSimpleWay(testData));
